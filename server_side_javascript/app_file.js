09cab496e39e3b08fd778ecb0b9df6141037d4f6
rexxxxxx <haroo0310@gmail.com>
2016-08-22 11:37:14 +0900

    Make a simple webapplication 2

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -18,7 +18,23 @@ app.get('/topic', function(req,res){
       res.status(500).send('Internal Server Error');
     }
     res.render('view',{topics:files});
-    
+
+  });
+});
+app.get('/topic/:id',function(req, res){
+  var id = req.params.id;
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+  fs.readFile('data/'+id, 'utf8', function(err, data){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('view',{topics:files, title:id, description:data});
+  })
   });
 });
 app.post('/topic',function(req,res){

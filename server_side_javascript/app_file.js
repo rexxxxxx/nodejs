287d0e72f980d9881b15608db11c5a38f57057c0
rexxxxxx <haroo0310@gmail.com>
2016-08-23 11:29:25 +0900

    File System

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -1,42 +1,85 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname);
+  }
+})
+
+var upload = multer({ storage : _storage })
 var fs = require('fs');
 var app = express();
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.locals.pretty= true;
+app.use('/user', express.static('uploads'));
+
+
+
 app.set('views','./views_file');
 app.set('view engine', 'jade');
 
-app.get('/topic/new', function(req,res){
-  res.render('new');
-})
-app.get('/topic', function(req,res){
-  fs.readdir('data', function(err, files){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.render('view',{topics:files});
+app.get('/upload', function(req, res){
+  res.render('upload');
 
-  });
 });
-app.get('/topic/:id',function(req, res){
-  var id = req.params.id;
+app.post('/upload', upload.single('userfile'), function(req, res){
+  console.log(req.file);
+  res.send('Uploaded : '+req.file.filename);
+});
+
+app.get('/topic/new', function(req,res){
   fs.readdir('data', function(err, files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-  fs.readFile('data/'+id, 'utf8', function(err, data){
+      res.render('new', {topics:files});
+  });
+
+});
+app.get(['/topic', '/topic/:id'], function(req,res){
+  fs.readdir('data', function(err, files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.render('view',{topics:files, title:id, description:data});
-  })
+    var id = req.params.id;
+    if(id){
+    // id 값이 있을 때.
+    fs.readFile('data/'+id, 'utf8', function(err, data){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+      res.render('view',{topics:files, title:id, description:data});
+    })
+  }else{
+    // id 값이 없을 때.
+    res.render('view',{topics:files, title:'Welcome', description:'Hello, JavaScript for Server'});
+  }
   });
 });
+// app.get('/topic/:id',function(req, res){
+//   var id = req.params.id;
+//   fs.readdir('data', function(err, files){
+//     if(err){
+//       console.log(err);
+//       res.status(500).send('Internal Server Error');
+//     }
+//   fs.readFile('data/'+id, 'utf8', function(err, data){
+//     if(err){
+//       console.log(err);
+//       res.status(500).send('Internal Server Error');
+//     }
+//     res.render('view',{topics:files, title:id, description:data});
+//   })
+//   });
+// });
 app.post('/topic',function(req,res){
   var title = req.body.title;
   var description = req.body.description;
@@ -45,7 +88,7 @@ app.post('/topic',function(req,res){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-    res.send('Success!');
+    res.redirect('/topic/'+title);
   });
 })
 
